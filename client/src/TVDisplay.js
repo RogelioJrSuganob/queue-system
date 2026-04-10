389b05d3fcdeade7aa95f25b3af3183399658122
Rogelio Suganob Jr. <60377421+RogelioJrSuganob@users.noreply.github.com>
2026-04-10 16:55:50 +0800

    UI update

--- client/src/TVDisplay.js
+++ client/src/TVDisplay.js
@@ -1,19 +1,43 @@
-import React, { useEffect, useState } from "react";
-import { socket } from "./socket";
+import React, { useEffect, useRef } from "react";
 
-export default function TVDisplay() {
-  const [data, setData] = useState({ number: 1, window: 1 });
+export default function TVDisplay({ queue }) {
+  const prevNumber = useRef(queue.number);
 
   useEffect(() => {
-    socket.on("queueUpdate", setData);
-    return () => socket.off("queueUpdate");
+    // Only play sound if number actually changed
+    if (queue.number !== prevNumber.current) {
+      playSound();
+      speak(queue.number, queue.window);
+      prevNumber.current = queue.number;
+    }
+  }, [queue]);
+
+  useEffect(() => {
+  console.log("Press F11 for fullscreen TV mode");
   }, []);
 
+  const playSound = () => {
+    const audio = new Audio("/ding.mp3");
+    audio.play();
+  };
+
+  const speak = (number, window) => {
+    const msg = new SpeechSynthesisUtterance(
+      `Now serving number ${number}, please proceed to window ${window}`
+    );
+    speechSynthesis.speak(msg);
+  };
+
   return (
-    <div style={{ textAlign: "center", fontSize: "50px" }}>
-      <h1>NOW SERVING</h1>
-      <div>{data.number}</div>
-      <div>WINDOW {data.window}</div>
+    <div className="tv">
+      <h1 className="tv-title">NOW SERVING</h1>
+
+      <div className="tv-number">{queue.number}</div>
+
+      <div className="tv-window">
+        WINDOW {queue.window}
+      </div>
     </div>
   );
+
 }
